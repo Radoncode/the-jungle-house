ad25de69839dfa78f5cc928f54b70345b2dfad65
RadonCode <relbouni69@gmail.com>
2021-03-20 19:48:10 +0000

    use a condition on the button

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -2,16 +2,15 @@ import { useState } from 'react';
 
 const QuestionForm = () => {
     const [inputValue, setInputValue] = useState('Ask your question here');
+    const isInputError = inputValue.includes('f');
 
-    const handleClick = () => {
-        alert(inputValue);
-    }
     return <div>
                 <textarea
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
                 />
-                <button onClick={handleClick}>Alert me 🚨</button>
+                <button onClick={() => isInputError ?
+       alert(`🔥 Vous n'avez pas le droit d'utiliser la lettre "f" ici`): alert(inputValue)}>Alert me 🚨</button>
     </div>
 }
 
